f474a6d897dc795ed900bcc9ab2e5d7c0b1fcd99
Thanasak Songsri <Thanasuksongsriohm@gmail.com>
2025-01-22 00:48:52 +0700

    Fix commit

--- src/sort.js
+++ src/sort.js
@@ -10,52 +10,49 @@ var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.merge = merge;
-function mergeSort(arr, left, mid, right) {
-    var n1 = mid - left + 1;
-    var n2 = right - mid;
-    var L = new Array(n1);
-    var R = new Array(n2);
-    console.log("L " + L.length);
-    for (var i_1 = 0; i_1 < n1; i_1++)
-        L[i_1] = arr[left + i_1];
-    for (var j_1 = 0; j_1 < n2; j_1++)
-        R[j_1] = arr[mid + 1 + j_1];
+function mainSort(arr, left, mid, right) {
+    var num1 = mid - left + 1;
+    var num2 = right - mid;
+    var setLeft = new Array(num1);
+    var setRight = new Array(num2);
+    for (var i_1 = 0; i_1 < num1; i_1++)
+        setLeft[i_1] = arr[left + i_1];
+    for (var j_1 = 0; j_1 < num2; j_1++)
+        setRight[j_1] = arr[mid + 1 + j_1];
     var i = 0, j = 0;
     var k = left;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            arr[k] = L[i];
+    while (i < num1 && j < num2) {
+        if (setLeft[i] <= setRight[j]) {
+            arr[k] = setLeft[i];
             i++;
         }
         else {
-            arr[k] = R[j];
+            arr[k] = setRight[j];
             j++;
         }
         k++;
     }
-    while (i < n1) {
-        arr[k] = L[i];
+    while (i < num1) {
+        arr[k] = setLeft[i];
         i++;
         k++;
     }
-    while (j < n2) {
-        arr[k] = R[j];
+    while (j < num2) {
+        arr[k] = setRight[j];
         j++;
         k++;
     }
 }
-function mainSort(arr, left, right) {
-    if (left >= right)
-        return;
-    var mid = Math.floor(left + (right - left) / 2);
-    mainSort(arr, left, mid);
-    mainSort(arr, mid + 1, right);
-    mergeSort(arr, left, mid, right);
-    return;
-}
 function merge(collection_1, collection_2, collection_3) {
     var result = __spreadArray(__spreadArray(__spreadArray([], collection_1, true), collection_2, true), collection_3, true);
-    mainSort(result, 0, result.length - 1);
+    var round = result.length;
+    for (var size = 1; size < round; size = 2 * size) {
+        for (var left = 0; left < round - 1; left += 2 * size) {
+            var mid = Math.min(left + size - 1, round - 1);
+            var right = Math.min(left + 2 * size - 1, round - 1);
+            mainSort(result, left, mid, right);
+        }
+    }
     return result;
 }
 console.log(merge([1, 2, 3], [4, 5, 6], [9, 8, 7]));
